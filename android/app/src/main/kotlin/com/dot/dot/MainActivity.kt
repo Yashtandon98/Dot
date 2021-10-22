36db8a38a13e9ece707549fc7623e3d53e99cfc0
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2021-10-22 22:30:44 +0530

    Added theme colors and partial theme service.

---
+++ android/app/src/main/kotlin/com/dot/dot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dot.dot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:dot/ui/home_page.dart';
+import 'package:dot/ui/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeMode.light,
+      home: HomePage()
+    );
+  }
+}
