6b8174156a13cbf18d3d77c602eada77902b47fc
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2021-10-22 22:17:03 +0530

    Added theme colors and partial theme service.

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Column(
+        children: [
+          Text('Theme Data',
+          style: TextStyle(
+            fontSize: 30
+          ),)
+        ],
+      ),
+    );
+  }
+
+  _appBar(){
+    return AppBar(
+      leading: InkWell(
+        onTap:(){
+
+        },
+        child: Icon(Icons.nightlight_round,
+        size: 20,),
+      ),
+      actions: [
+        Icon(Icons.person,
+        size: 20,),
+        SizedBox(width: 20,)
+      ],
+    );
+  }
+}
