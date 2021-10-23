6b428c3662e85149bd4a7cc7beac34d91deb0ccf
Yashtandon98 <40264382+Yashtandon98@users.noreply.github.com>
2021-10-23 21:51:51 +0530

    Added create task page with color palette.

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -1,7 +1,10 @@
+
 import 'package:dot/ui/theme.dart';
+import 'package:dot/ui/widgets/button.dart';
 import 'package:dot/ui/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class AddTaskPage extends StatefulWidget {
   const AddTaskPage({Key? key}) : super(key: key);
@@ -11,6 +14,26 @@ class AddTaskPage extends StatefulWidget {
 }
 
 class _AddTaskPageState extends State<AddTaskPage> {
+
+  DateTime _selectedDate = DateTime.now();
+  String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+  String _endTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+  int _selectedRemind = 5;
+  List<int> remindList = [
+    5,
+    10,
+    15,
+    20,
+  ];
+  String _selectedRepeat = "None";
+  List<String> repeatList = [
+    "None",
+    "Daily",
+    "Weekly",
+    "Monthly",
+  ];
+  int _selectedColor=0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,13 +49,147 @@ class _AddTaskPageState extends State<AddTaskPage> {
               style: headingStyle,),
               MyInputField(title: "Title", hint: "Enter your title"),
               MyInputField(title: "Note", hint: "Enter your note"),
-              MyInputField(title: "Title", hint: "Enter your title"),
-              MyInputField(title: "Title", hint: "Enter your title"),
-              MyInputField(title: "Title", hint: "Enter your title"),
+              MyInputField(title: "Date", hint: DateFormat.yMd().format(_selectedDate),
+                widget: IconButton(
+                    onPressed: (){
+                      _getDateFromUser();
+                    },
+                    icon: Icon(Icons.calendar_today_outlined,
+                    color: Colors.grey,
+                    ),
+                ),),
+              Row(
+                children: [
+                  Expanded(
+                      child: MyInputField(
+                        title: "Start Date",
+                        hint: _startTime,
+                        widget: IconButton(
+                            onPressed: (){
+                              _getTimeFromUser(isStartTime: true);
+                            },
+                            icon: Icon(
+                              Icons.access_time_rounded,
+                              color: Colors.grey,
+                            ),
+                        ),
+                      ),
+                  ),
+                  SizedBox(width: 12,),
+                  Expanded(
+                    child: MyInputField(
+                      title: "End Date",
+                      hint: _endTime,
+                      widget: IconButton(
+                        onPressed: (){
+                          _getTimeFromUser(isStartTime: false);
+                        },
+                        icon: Icon(
+                          Icons.access_time_rounded,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              MyInputField(title: "Remind", hint: "$_selectedRemind minutes early",
+                widget: DropdownButton(
+                  icon: Icon(Icons.keyboard_arrow_down,
+                  color: Colors.grey,
+                  ),
+                  iconSize: 32,
+                  elevation: 4,
+                  style: subTitleStyle,
+                  underline: Container(height: 0,),
+                  onChanged: (String? newValue){
+                    setState(() {
+                      _selectedRemind = int.parse(newValue!);
+                    });
+                  },
+                  items: remindList.map<DropdownMenuItem<String>>((int? value){
+                    return DropdownMenuItem<String>(
+                      value: value.toString(),
+                      child: Text(value.toString()),
+                    );
+                  }
+                  ).toList(),
+                ),
+              ),
+              MyInputField(title: "Repeat", hint: "$_selectedRepeat",
+                widget: DropdownButton(
+                  icon: Icon(Icons.keyboard_arrow_down,
+                    color: Colors.grey,
+                  ),
+                  iconSize: 32,
+                  elevation: 4,
+                  style: subTitleStyle,
+                  underline: Container(height: 0,),
+                  onChanged: (String? newValue){
+                    setState(() {
+                      _selectedRepeat = newValue!;
+                    });
+                  },
+                  items: repeatList.map<DropdownMenuItem<String>>((String? value){
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value!, style: TextStyle(color: Colors.grey),),
+                    );
+                  }
+                  ).toList(),
+                ),
+              ),
+              SizedBox(height: 18,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _colorPallete(),
+                  MyButton(label: "Create Task", onTap: () => null)
+                ],
+              ),
+              SizedBox(height: 30,),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  _colorPallete(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Color",
+          style: titleStyle,
+        ),
+        SizedBox(height: 8.0,),
+        Wrap(
+          children: List<Widget>.generate(
+              3,
+                  (int index){
+                return InkWell(
+                  onTap: (){
+                    setState(() {
+                      _selectedColor = index;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: CircleAvatar(
+                      radius: 14,
+                      backgroundColor: index == 0?primaryClr:index==1?pinkClr:yellowClr,
+                      child: _selectedColor==index?Icon(Icons.done,
+                        color: Colors.white,
+                        size: 16,
+                      ):Container(),
+                    ),
+                  ),
+                );
+              }
+          ),
+        ),
+      ],
     );
   }
 
@@ -60,4 +217,48 @@ class _AddTaskPageState extends State<AddTaskPage> {
     );
   }
 
+  _getDateFromUser() async{
+    DateTime? _pickerDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2121)
+    );
+
+    if(_pickerDate != null){
+      setState(() {
+        _selectedDate = _pickerDate;
+        print(_selectedDate);
+      });
+    }else{
+      print("It's null or something is wrong");
+    }
+  }
+
+  _getTimeFromUser({required bool isStartTime}) async{
+    var _pickedTime = await _showTimePicker();
+    String _formatedTime = _pickedTime.format(context);
+    if(_pickedTime == null){
+      print("Time canceled");
+    }else if(isStartTime == true){
+      setState(() {
+        _startTime = _formatedTime;
+      });
+    }else if(isStartTime == false){
+      setState(() {
+        _endTime = _formatedTime;
+      });
+    }
+  }
+
+  _showTimePicker(){
+    return showTimePicker(
+        initialEntryMode: TimePickerEntryMode.input,
+        context: context,
+        initialTime: TimeOfDay(
+            hour: int.parse(_startTime.split(":")[0]),
+            minute: int.parse(_startTime.split(":")[1].split(" ")[0])
+        )
+    );
+  }
 }
